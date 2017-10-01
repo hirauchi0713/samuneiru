3fd5a4c50a3b85c20e123b44a9269fddf9f665e7
hirauchi <hirauchi@tridentworks.co.jp>
2017-10-01 16:28:12 +0900

    init

---
+++ README.md
@@ -0,0 +1,48 @@
+samuneiru (thumbnail) Ver.1.0.0
+===============================
+
+これはなに？
+------------
+
+画像ファイルをリサイズします。入力ファイルのフォーマットはjpg,png,gifから自動判定します。
+
+使い方
+------
+
+```
+# 標準入力の画像データを幅500pxにして標準出力へ
+$ cat input.png | samuneiru -width 500 > output.png
+
+# ファイル名指定版
+$ samuneiru -width 500 -ifile input.png -ofile output.png
+
+# 出力フォーマットをjpgに
+$ samuneiru -width 500 -ifile input.png -ofile output.jpg -oformat jpg
+
+# HELP
+$ samuneiru -help
+samuneiru: The image thumbnailer. (Ver.1.0.0)
+usage: ./samuneiru [-width | -height] [other options]
+  -height uint
+      resize height. (0 is keep aspect ratio)
+  -help
+      print this help message.
+  -ifile string
+      input-file name. ('-' is stdin)  (default "-")
+  -ofile string
+      output-file name. ('-' is stdout)  (default "-")
+  -oformat string
+      output-file format. [jpg|png|gif] ('-' is same as input) (default "-")
+  -width uint
+      resize width. (0 is keep aspect ratio)
+```
+
+License
+-------
+
+MIT
+
+Author
+------
+
+Hideyuki Hirauchi &lt;hirauchi@ideapump.net&gt;
---
+++ samuneiru.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/nfnt/resize"
+	"github.com/soniakeys/quant/median"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"log"
+	"os"
+)
+
+const VERSION = "1.0.0"
+
+func Resize(ifile string, ofile string, width uint, height uint, oformat string) int {
+	//
+	// input
+	//
+	var in io.Reader
+	var err error
+	if ifile == "-" {
+		in = os.Stdin
+	} else {
+		in, err = os.Open(ifile)
+		if err != nil {
+			log.Fatal(err)
+			return 1
+		}
+	}
+
+	//
+	// decode
+	//
+	image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
+	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
+	image.RegisterFormat("gif", "gif", gif.Decode, gif.DecodeConfig)
+	img, fmt, err := image.Decode(in)
+	if err != nil {
+		log.Fatal(err)
+		return 1
+	}
+
+	//
+	// resize
+	//
+	m := resize.Resize(width, height, img, resize.Bicubic)
+
+	//
+	// output
+	//
+	var out io.Writer
+	if ofile == "-" {
+		out = os.Stdout
+	} else {
+		out, err = os.Create(ofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err != nil {
+		log.Fatal(err)
+		return 1
+	}
+
+	if oformat == "-" {
+		oformat = fmt
+	}
+	if oformat == "png" {
+		png.Encode(out, m)
+	} else if oformat == "gif" {
+		opt := gif.Options{}
+		opt.NumColors = 256
+		opt.Quantizer = median.Quantizer(256)
+		gif.Encode(out, m, &opt)
+	} else {
+		opt := jpeg.Options{}
+		opt.Quality = 100
+		jpeg.Encode(out, m, &opt)
+	}
+
+	return 0
+}
+
+func main() {
+	//
+	// option
+	//
+	printUsage := flag.Bool("help", false, "print this help message.")
+	ifile := flag.String("ifile", "-", "input-file name. ('-' is stdin) ")
+	ofile := flag.String("ofile", "-", "output-file name. ('-' is stdout) ")
+	oformat := flag.String("oformat", "-", "output-file format. [jpg|png|gif] ('-' is same as input)")
+	width := flag.Uint("width", 0, "resize width. (0 is keep aspect ratio)")
+	height := flag.Uint("height", 0, "resize height. (0 is keep aspect ratio)")
+	flag.Parse()
+
+	if *printUsage {
+		fmt.Fprintf(os.Stderr, "samuneiru: The image thumbnailer. (Ver.%s)\n", VERSION)
+		fmt.Fprintf(os.Stderr, "usage: %s [-width | -height] [other options]\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if *width == 0 && *height == 0 {
+		log.Println("You must set -width or -height")
+		flag.Parse()
+		os.Exit(2)
+	}
+
+	if *width != 0 && *height != 0 {
+		log.Println("You can't set both -width and -height")
+		flag.Parse()
+		os.Exit(2)
+	}
+
+	os.Exit(Resize(*ifile, *ofile, *width, *height, *oformat))
+}
